032e2c5b12b45b5fbb8f9c87d7d5b7204fb38429
Samuel Cardoso <scksamuel.sc@gmail.com>
2018-12-24 05:36:07 -0300

    Load Main menu from Language selection menu

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,17 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
+using UnityEngine.SceneManagement;
 
 public class GameManager : SingletonMonoBehaviour<GameManager>
 {
     //THE language manager
     LanguageReader langReader;
-	//Game language
+    //Game language
     Language lang = Language.English;
     //path of the file that the game is reading from
     string langFilePath = "Resources/menuSentences.xml";
 
-	protected override void Awake()
+    protected override void Awake()
     {
         base.isDontDestroy = true;
         base.Awake();
@@ -20,29 +21,34 @@ public class GameManager : SingletonMonoBehaviour<GameManager>
         langReader = new LanguageReader(Path.Combine(Application.dataPath, langFilePath), lang.Value, false);
     }
 
-	//Load a scene
-    public void LoadScene(string sceneName) { }
+    //Load a scene
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
 
     //Set path of the file with the sentences
-    public void SetLanguageFile(string filePath){
+    public void SetLanguageFile(string filePath)
+    {
         langReader.setLanguage(Path.Combine(Application.dataPath, filePath), lang.Value);
         langFilePath = filePath;
     }
 
     //Get sentence from file
     //the names of the sentences are defined in the xml file
-    public string GetSentence(string sentenceName){
+    public string GetSentence(string sentenceName)
+    {
         return langReader.getString(sentenceName);
     }
 
-	//Set game language
+    //Set game language
     public void SetLanguage(Language _language)
     {
         langReader.setLanguage(Path.Combine(Application.dataPath, langFilePath), _language.Value);
         lang = _language;
     }
 
-	//Get game language
+    //Get game language
     public Language GetLanguage()
     {
         return lang;
